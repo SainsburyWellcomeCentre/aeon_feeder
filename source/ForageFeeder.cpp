d66bc4bb77a7ccd8a6b556233b62cf61bb8a2073
gmcphillips <g.mcphillips@ucl.ac.uk>
2021-10-31 17:49:54 +0000

    starting to work out velocity

--- source/ForageFeeder.cpp
+++ source/ForageFeeder.cpp
@@ -30,6 +30,8 @@
 #define ROTARY_SW           7
 #define ROTARY_A            8
 #define ROTARY_B            9
+#define ROTARY_RIGHT        1
+#define ROTARY_LEFT         0
 #if PWM_ENABLE
 #define PWM_IN_ONE          10
 #define PWM_IN_TWO          11
@@ -45,37 +47,71 @@
 #define GPIO_ON             1
 #define GPIO_OFF            0
 
-/* Controller parameters */
-#define PID_KP  2.0f
-#define PID_KI  0.5f
-#define PID_KD  0.25f
+#define LEVEL_LOW           0x1
+#define LEVEL_HIGH          0x2
+#define EDGE_FALL           0x4
+#define EDGE_RISE           0x8
 
-#define PID_TAU 0.02f
+/* Velocity Controller parameters */
+#define PID_VEL_KP  2.0f
+#define PID_VEL_KI  0.5f
+#define PID_VEL_KD  0.25f
 
-#define PID_LIM_MIN -10.0f
-#define PID_LIM_MAX  10.0f
+#define PID_VEL_TAU 0.02f
 
-#define PID_LIM_MIN_INT -5.0f
-#define PID_LIM_MAX_INT  5.0f
+#define PID_VEL_LIM_MIN -10.0f
+#define PID_VEL_LIM_MAX  10.0f
 
-#define SAMPLE_TIME_S 0.01f
+#define PID_VEL_LIM_MIN_INT -5.0f
+#define PID_VEL_LIM_MAX_INT  5.0f
+
+#define SAMPLE_TIME_VEL_S 0.001f
+
+/* Position Controller parameters */
+#define PID_POS_KP  2.0f
+#define PID_POS_KI  0.5f
+#define PID_POS_KD  0.25f
+
+#define PID_POS_TAU 0.02f
+
+#define PID_POS_LIM_MIN -10.0f
+#define PID_POS_LIM_MAX  10.0f
+
+#define PID_POS_LIM_MIN_INT -5.0f
+#define PID_POS_LIM_MAX_INT  5.0f
+
+#define SAMPLE_TIME_POS_S 0.001f
 
 using namespace pimoroni;
 
 uint16_t buffer[BreakoutColourLCD240x240::WIDTH * BreakoutColourLCD240x240::HEIGHT];
 BreakoutColourLCD240x240 lcd(buffer);
 
-PIDController pid = { PID_KP, PID_KI, PID_KD,
-                        PID_TAU,
-                        PID_LIM_MIN, PID_LIM_MAX,
-                        PID_LIM_MIN_INT, PID_LIM_MAX_INT,
-                        SAMPLE_TIME_S };
+PIDController pid_vel = { PID_VEL_KP, PID_VEL_KI, PID_VEL_KD,
+                        PID_VEL_TAU,
+                        PID_VEL_LIM_MIN, PID_VEL_LIM_MAX,
+                        PID_VEL_LIM_MIN_INT, PID_VEL_LIM_MAX_INT,
+                        SAMPLE_TIME_VEL_S };
+
+PIDController pid_pos = { PID_POS_KP, PID_POS_KI, PID_POS_KD,
+                        PID_POS_TAU,
+                        PID_POS_LIM_MIN, PID_POS_LIM_MAX,
+                        PID_POS_LIM_MIN_INT, PID_POS_LIM_MAX_INT,
+                        SAMPLE_TIME_POS_S };
 
 struct repeating_timer control_loop;
 
+volatile bool rotary_direction;
+volatile bool rotary_int_flag;
+volatile bool rotary_rotation_flag;
+volatile bool rotary_switch_In_flag;
+volatile bool rotary_switch_Out_flag;
 volatile bool motor_direction;
 int motor_postion = 0;
 int motor_postion_old = 0;
+int motor_speed = 0;
+int motor_speed_old = 0;
+int velocity_setpoint = 0;
 uint16_t set_pwm_one = 0;
 uint16_t set_pwm_two = 0;
 volatile bool pellet_delivered;
@@ -91,16 +127,72 @@ void vApplicationTask( void * pvParameters );
 void vUpdateScreenTask( void * pvParameters );
 
 void gpio_event_string(char *buf, uint32_t events);
+void initialise_feeder(void);
 
 // Interupt Callback Routines - START
+// GPIO Callback for Core 1
 void gpio_callback_core_1(uint gpio, uint32_t events) {
-    // Put the GPIO event(s) that just happened into event_str
-    // so we can print it
-    gpio_event_string(event_str, events);
-    sprintf(lcd_message_str, "GPIO %d %s\n", gpio, event_str);
-    printf("GPIO %d %s\n", gpio, event_str);
+    switch(gpio) {
+        case ROTARY_A:
+            switch(events) {
+                case EDGE_FALL:
+                    if (rotary_int_flag){
+                        rotary_direction = ROTARY_LEFT;
+                        rotary_rotation_flag = true;
+                        printf("Rotary Left\n");
+                    } else {
+                        rotary_int_flag = true;
+                    }
+                    break;
+                case EDGE_RISE:
+                    rotary_int_flag = false;
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case ROTARY_B:
+            switch(events) {
+                case EDGE_FALL:
+                    if (rotary_int_flag){
+                        rotary_direction = ROTARY_RIGHT;
+                        rotary_rotation_flag = true;
+                        printf("Rotary Right\n");
+                    } else {
+                        rotary_int_flag = true;
+                    }
+                    break;
+                case EDGE_RISE:
+                    rotary_int_flag = false;
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case ROTARY_SW:
+            switch(events) {
+                case EDGE_FALL:
+                    rotary_switch_In_flag = true;
+                    printf("Rotary Switch IN\n");
+                    break;
+                case EDGE_RISE:
+                    rotary_switch_Out_flag = true;
+                    printf("Rotary Switch OUT\n");
+                    break;
+                default:
+
+                    break;
+            }
+            
+            // gpio_event_string(event_str, events);
+            // printf("GPIO %d %s\n", gpio, event_str);
+            break;
+        default:
+            break;
+    }
 }
 
+// GPIO Callback for Core 0
 void gpio_callback_core_0(uint gpio, uint32_t events) {
 
     switch(gpio) {
@@ -125,6 +217,7 @@ void gpio_callback_core_0(uint gpio, uint32_t events) {
 #endif
         case BEAM_BREAK_PIN:
             pellet_delivered = true;
+            set_pwm_one = 0;    // NB must remove this later after testing.....
             gpio_event_string(event_str, events);
             printf("GPIO %d %s\n", gpio, event_str);
             break;
@@ -132,20 +225,19 @@ void gpio_callback_core_0(uint gpio, uint32_t events) {
 
             break;
     }
-
 }
 
+// Timer Callback for 1ms Control Loop.
 bool repeating_timer_callback(struct repeating_timer *t) {
-    if (LED_TOGGLE){
-        gpio_put(GREEN_LED_PIN, GPIO_OFF);
-        LED_TOGGLE = !LED_TOGGLE;
-    } else {
-        gpio_put(GREEN_LED_PIN, GPIO_ON);
-        LED_TOGGLE = !LED_TOGGLE;
-    }
-
-
-    // printf("Repeat at %lld\n", time_us_64());
+    // if (LED_TOGGLE){
+    //     gpio_put(GREEN_LED_PIN, GPIO_OFF);
+    //     LED_TOGGLE = !LED_TOGGLE;
+    // } else {
+    //     gpio_put(GREEN_LED_PIN, GPIO_ON);
+    //     LED_TOGGLE = !LED_TOGGLE;
+    // }
+    motor_speed = motor_postion - motor_postion_old;
+    motor_postion_old = motor_postion;
     return true;
 }
 
@@ -154,6 +246,18 @@ void on_pwm_wrap() {
 
     pwm_clear_irq(pwm_gpio_to_slice_num(PWM_IN_ONE));
 
+    if (velocity_setpoint == 0) {
+        set_pwm_two = 0;
+        set_pwm_one = 0;
+    } else if (velocity_setpoint > 0) {
+        set_pwm_one = velocity_setpoint;
+        if (set_pwm_one > 4095) set_pwm_one = 4095;
+        set_pwm_two = 0;
+    } else {
+        set_pwm_two = velocity_setpoint*-1;
+        if (set_pwm_two > 4095) set_pwm_two = 4095;
+        set_pwm_one = 0;
+    }
     pwm_set_gpio_level(PWM_IN_ONE, set_pwm_one);
     pwm_set_gpio_level(PWM_IN_TWO, set_pwm_two);
 }
@@ -175,8 +279,8 @@ void swc_base() {
     printf("Hello GPIO IRQ\n");
     sprintf(lcd_message_str, "Hello GPIO IRQ");
     gpio_set_irq_enabled_with_callback(ROTARY_SW, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback_core_1);
-    gpio_set_irq_enabled_with_callback(ROTARY_A, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_core_1);
-    gpio_set_irq_enabled_with_callback(ROTARY_B, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_core_1);
+    gpio_set_irq_enabled_with_callback(ROTARY_A, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback_core_1);
+    gpio_set_irq_enabled_with_callback(ROTARY_B, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback_core_1);
 
     lcd.init();
     lcd.set_backlight(255);
@@ -270,9 +374,12 @@ int main()
                   tskIDLE_PRIORITY + 1,           // Task Priority
                   &xUpdateScreenHandle );  
 
-    add_repeating_timer_us(-1000, repeating_timer_callback, NULL, &control_loop);
+    // Add control loop timer 
+    add_repeating_timer_us(-1000, repeating_timer_callback, NULL, &control_loop);   // 1ms
+    add_repeating_timer_ms(-10, repeating_timer_callback, NULL, &control_loop);   // 10ms
 
-    PIDController_Init(&pid);
+    PIDController_Init(&pid_vel);
+    PIDController_Init(&pid_pos);
 
     vTaskStartScheduler();
 
@@ -294,12 +401,30 @@ void vGreenLEDTask( void * pvParameters )
 
 void vApplicationTask( void * pvParameters )
 {
+    // initialise_feeder();     // commented out while doing the control loops
+
     for( ;; )
     {
         // gpio_put(GREEN_LED_PIN, GPIO_ON);
-        vTaskDelay(1);
+        if (rotary_rotation_flag) {
+            if (rotary_direction) {
+                velocity_setpoint += 250;
+            } else {
+                velocity_setpoint -= 250;
+            }
+            rotary_rotation_flag = false;
+        }
+        if (rotary_switch_In_flag) {
+            velocity_setpoint = 4095;
+            rotary_switch_In_flag = false;
+        }
+        if (rotary_switch_Out_flag) {
+            velocity_setpoint = 0;
+            rotary_switch_Out_flag = false;
+        }
+        vTaskDelay(10);
         // gpio_put(GREEN_LED_PIN, GPIO_OFF);
-        vTaskDelay(1);
+        // vTaskDelay(1);
     }
 }
 
@@ -307,11 +432,11 @@ void vUpdateScreenTask( void * pvParameters )
 {
     for( ;; )
     {
-        if (motor_postion != motor_postion_old){
-            printf("Position %d\n", motor_postion);
+        if (motor_speed != motor_speed_old){
+            printf("Speed %d\n", motor_speed);
         }
-        motor_postion_old = motor_postion;
-        vTaskDelay(100);
+        motor_speed_old = motor_speed;
+        vTaskDelay(10);
     }
 }
 
@@ -334,4 +459,14 @@ void gpio_event_string(char *buf, uint32_t events) {
         }
     }
     *buf++ = '\0';
+}
+
+void initialise_feeder(void) {
+    set_pwm_one = 150;
+    while(!pellet_delivered){
+        vTaskDelay(1);
+    }
+    set_pwm_one = 0;
+    pellet_delivered = false;
+
 }
